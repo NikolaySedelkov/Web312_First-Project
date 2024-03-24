95c1baba67ebd420dfd3f9eb09e10483b03924ab
Nikolay <sedelkov.nikolay@gmail.com>
2024-03-24 12:49:19 +0300

    dafualt props SizeBlock

--- src/components/SizeBlock/SizeBlock.jsx
+++ src/components/SizeBlock/SizeBlock.jsx
@@ -1,6 +1,6 @@
 import "./SizeBlock.css";
 
-export default function SizeBlock({width, height}) {
+export default function SizeBlock({width = 0, height = 0}) {
     return (
         <div 
             className="red-block"
